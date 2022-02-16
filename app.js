dc1eef32b17e40e582a0055afba6c28514bc3cab
Joe Scott <jbs198585@gmail.com>
2022-02-16 06:50:42 -0500

    Functions with multiple arguments

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ function singSong() {
   console.log("MI");
 }
 
-function greet(firstName) {
-  console.log(`Hey there, ${firstName}`);
+function greet(firstName, lastName) {
+  console.log(`Hey there, ${firstName} ${lastName[0]}.`);
+}
+
+function repeat(str, numTimes) {
+  let result = "";
+  for (let i = 0; i < numTimes; i++) {
+    result += str;
+  }
+  console.log(result);
 }
